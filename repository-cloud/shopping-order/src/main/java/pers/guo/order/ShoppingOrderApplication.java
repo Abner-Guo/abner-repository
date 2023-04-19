60b0ffd63a7e0748a7820796c4a3359196e7fe40
guochao.bj <guochao.bj@fang.com>
2023-04-19 20:19:41 +0800

    repository-cloud add nacos

--- repository-cloud/shopping-order/src/main/java/pers/guo/order/ShoppingOrderApplication.java
+++ repository-cloud/shopping-order/src/main/java/pers/guo/order/ShoppingOrderApplication.java
@@ -2,8 +2,11 @@ package pers.guo.order;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class ShoppingOrderApplication {
 
     public static void main(String[] args) {
--- repository-cloud/shopping-user/src/main/java/pers/guo/user/ShoppingUserApplication.java
+++ repository-cloud/shopping-user/src/main/java/pers/guo/user/ShoppingUserApplication.java
@@ -2,8 +2,10 @@ package pers.guo.user;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class ShoppingUserApplication {
 
     public static void main(String[] args) {
