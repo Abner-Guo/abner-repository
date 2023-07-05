f95101274c5e25d36a706b47e55476e7016bcdfa
guochao.bj <guochao.bj@fang.com>
2023-07-05 08:04:01 +0800

    set gateway order

--- repository-cloud/shopping-gateway/src/main/java/pers/guo/gateway/filter/HttpGlobalFilter.java
+++ repository-cloud/shopping-gateway/src/main/java/pers/guo/gateway/filter/HttpGlobalFilter.java
@@ -2,6 +2,7 @@ package pers.guo.gateway.filter;
 
 import org.springframework.cloud.gateway.filter.GatewayFilterChain;
 import org.springframework.cloud.gateway.filter.GlobalFilter;
+import org.springframework.core.Ordered;
 import org.springframework.stereotype.Component;
 import org.springframework.web.server.ServerWebExchange;
 import reactor.core.publisher.Mono;
@@ -14,7 +15,7 @@ import reactor.core.publisher.Mono;
  * @createDate: 2023/7/3 19:29
  */
 @Component
-public class HttpGlobalFilter implements GlobalFilter {
+public class HttpGlobalFilter implements GlobalFilter, Ordered {
 
 
 
@@ -25,5 +26,15 @@ public class HttpGlobalFilter implements GlobalFilter {
         System.out.println("【gateway 全局过滤器】"+exchange.getRequest().getPath().value());
 
         return chain.filter(exchange);
+    }
+
+    /**
+     * 设置过滤器优先级
+     * @author guochao.bj@fang.com
+     * @date 2023/7/4
+     */
+    @Override
+    public int getOrder() {
+        return 0;
     }
 }
