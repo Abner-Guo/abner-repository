d29d56f5b8a5a70ae211ed69acf096a4c34539d1
guochao.bj <guochao.bj@fang.com>
2023-07-08 14:08:23 +0800

    set i/o module

---
+++ repository-template/src/main/java/pers/guo/repositorytemplate/design/package-info.java
@@ -0,0 +1,10 @@
+/**
+ * 设计模式相关测试DEMO
+ *
+ *
+ *
+ *
+ */
+package pers.guo.repositorytemplate.design;
+
+
---
+++ repository-template/src/main/java/pers/guo/repositorytemplate/sysio/package-info.java
@@ -0,0 +1,10 @@
+/**
+ * 系统I/O相关的代码测试
+ *
+ *
+ *
+ *
+ */
+package pers.guo.repositorytemplate.sysio;
+
+
