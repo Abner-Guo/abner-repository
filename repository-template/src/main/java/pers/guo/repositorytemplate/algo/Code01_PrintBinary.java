71ad9993444c740a01cac2db9448e712ff08efbe
guochao.bj <guochao.bj@fang.com>
2023-08-17 19:28:34 +0800

    algo

---
+++ repository-template/src/main/java/pers/guo/repositorytemplate/algo/Code01_PrintBinary.java
@@ -0,0 +1,33 @@
+package pers.guo.repositorytemplate.algo;
+
+/**
+ * @author: guochao.bj@fang.com
+ * @createDate: 2023/8/17 18:36
+ */
+public class Code01_PrintBinary {
+
+    /**
+     * 1.二进制打印整数，Int 是32位
+     * 2.位运算 ，乘2是左移1位
+     * 3.为什么范围是-2^31 到 2^31-1
+     * 4.二进制取反~5
+     * 5.负数二进制是32位符号位，剩下的31位取反加1
+     * 6.正数取反+1 是负数
+     * 5.最小的负数，取相反数还是本身
+     */
+
+    public static void point(int num){
+
+        for (int i = 31; i >=0 ; i--) {
+            System.out.print((num&(1<<i))==0?"0":"1");
+        }
+        System.out.println();
+    }
+
+
+    public static void main(String[] args) {
+        point(Integer.MIN_VALUE);
+    }
+
+
+}
---
+++ repository-template/src/main/java/pers/guo/repositorytemplate/algo/package-info.java
@@ -0,0 +1,8 @@
+/**
+ * AlgoJourney: 算法之旅，记录自己的算法学习历程和心得。
+ * https://shimo.im/docs/68cd6h3TwygPwx8W/read
+ * https://cloud.fynote.com/share/d/5280/29858
+ * https://github.com/algorithmzuo
+ *
+ */
+package pers.guo.repositorytemplate.algo;
